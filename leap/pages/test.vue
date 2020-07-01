6a9b8422f66f06e52c321e5828e8ca3524094a9f
anilkumarggk <anilkumarggk@gmail.com>
2020-07-01 13:58:46 +0530

    Add test page to test out the components and styling - go-to playground

---
+++ leap/pages/test.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Testing page</h1>
+    <b-btn variant="danger">Error button</b-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'app-page'
+}
+</script>

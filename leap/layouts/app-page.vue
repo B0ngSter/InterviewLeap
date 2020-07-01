108860387431e9acd083e12f6b8d09775cee11cf
anilkumarggk <anilkumarggk@gmail.com>
2020-07-01 13:58:20 +0530

    Add new layout: app related pages are expected to use this layout

---
+++ leap/layouts/app-page.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <b-navbar variant="faded" type="light">
+      <b-navbar-brand to="/">
+        <Logo />
+      </b-navbar-brand>
+    </b-navbar>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo'
+export default {
+  components: {
+    Logo
+  }
+}
+</script>

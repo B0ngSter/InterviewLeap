9a43a40f912d187a1ca70eda60c8efc0450302fa
k-sweta <sweta@stockroom.io>
2020-07-06 10:54:25 +0530

    Interviewleap: signup view, url modified, removed required_field from models.py, login end point added for traditional login and google login

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -4,18 +4,10 @@ from django.contrib.auth.admin import UserAdmin
 
 
 class CustomUserAdmin(UserAdmin):
-    UserAdmin.fieldsets += ('Custom fields set', {'fields': ('email_verified', 'role', 'profile_picture')}),
+    UserAdmin.fieldsets += ('Custom fields set', {'fields': ('role',)}),
 
     model = User
     readonly_fields = ['date_joined']
-
-
-class CompanyProfileAdmin(admin.ModelAdmin):
-
-    def get_queryset(self, request):
-        queryset = super(CompanyProfileAdmin, self).get_queryset(request)
-        queryset = queryset.order_by('-created_at')
-        return queryset
 
 
 admin.site.register(User, CustomUserAdmin)

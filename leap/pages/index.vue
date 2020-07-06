7d3769125eb5c2877b604810fe2832288c3774e1
anilkumarggk <anilkumarggk@gmail.com>
2020-07-06 12:21:50 +0530

    update test pages

--- leap/pages/index.vue
+++ leap/pages/index.vue
@@ -30,14 +30,12 @@ export default {}
 
 .title {
   display: block;
-  font-weight: 300;
   font-size: 100px;
   color: #35495e;
   letter-spacing: 1px;
 }
 
 .subtitle {
-  font-weight: 300;
   font-size: 42px;
   color: #526488;
   word-spacing: 5px;

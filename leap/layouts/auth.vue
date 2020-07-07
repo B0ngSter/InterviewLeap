00465e98e674b893edef77b6dd232361a8500adf
G0dlikecoder <55620900+G0dlikecoder@users.noreply.github.com>
2020-07-08 01:06:21 +0530

    auth 1 with google_auth minus few minor adjustments

---
+++ leap/layouts/auth.vue
@@ -0,0 +1,10 @@
+<template>
+<div>
+    <b-navbar toggleable="lg">
+        <b-navbar-brand href="#">
+        <Logo />
+        </b-navbar-brand>
+    </b-navbar>
+    <Nuxt />
+</div>
+</template>

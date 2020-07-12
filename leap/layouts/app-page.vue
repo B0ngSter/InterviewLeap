7006b4cda689167721fc9fe7bbe989334d93107b
anilkumarggk <anilkumarggk@gmail.com>
2020-07-13 00:30:39 +0530

    Update app pages layout

--- leap/layouts/app-page.vue
+++ leap/layouts/app-page.vue
@@ -1,31 +1,33 @@
 <template>
-  <b-container fluid="">
-    <b-row no-gutters>
-      <b-navbar variant="light" type="light" fixed="top">
-        <b-navbar-brand to="/">
-          <Logo />
-        </b-navbar-brand>
-      </b-navbar>
-    </b-row>
-    <b-row no-gutters class="mt-84">
-      <b-col cols="12">
-        <Nuxt />
-      </b-col>
-    </b-row>
-  </b-container>
+  <section>
+    <Navbar />
+    <b-container fluid="" class="bg-light page-content">
+      <b-row no-gutters class="justify-content-center">
+        <b-col cols="12" md="10">
+          <Nuxt />
+        </b-col>
+      </b-row>
+    </b-container>
+  </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo'
+import Navbar from '~/components/app-navbar'
+
 export default {
   components: {
-    Logo
+    Navbar
   }
 }
 </script>
 
 <style scoped>
-.mt-84 {
-  padding-top: 84px;
+.container {
+  margin: 0 auto;
+  min-height: calc(100vh - 84px);
+  display: flex;
+}
+.page-content {
+  margin-top: 84px;
 }
 </style>

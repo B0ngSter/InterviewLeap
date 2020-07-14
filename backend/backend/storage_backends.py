cda95337dbbf99d96870f7b28ab29638cfee9f28
madhutsm <madhu@stockroom.io>
2020-07-14 10:56:34 +0530

    S3 bucket class file added

---
+++ backend/backend/storage_backends.py
@@ -0,0 +1,14 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class PrivateMediaStorage(S3Boto3Storage):
+    location = 'private'
+    default_acl = 'private'
+    file_overwrite = False
+    custom_domain = False
+
+
+class PublicMediaStorage(S3Boto3Storage):
+    location = 'public'
+    default_acl = 'public-read'
+    file_overwrite = False

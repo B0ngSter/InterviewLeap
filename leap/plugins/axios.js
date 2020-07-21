f05b857bd178203e2b95e681df701fc8988d2be4
G0dlikecoder <55620900+G0dlikecoder@users.noreply.github.com>
2020-07-21 13:24:19 +0530

    minor: removed a debugger and string should be singlequote error

--- leap/plugins/axios.js
+++ leap/plugins/axios.js
@@ -1,9 +1,9 @@
 export default function ({ store, $axios, redirect }) {
   $axios.onError((error) => {
     if (error.response.status === 401) {
-      console.log("Login to continue")
+      console.log('Login to continue')
       // Logout here and redirect to login page
-      redirect("/auth/login")
+      redirect('/auth/login')
     }
   })
 }

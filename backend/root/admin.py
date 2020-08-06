552e9966cec1cc5e999d93652ad6fe8837008004
k-sweta <sweta@stockroom.io>
2020-08-06 21:16:13 +0530

    Payment Integration: Api's payment request response, Payment keys added in Config, mock listing(candidate dashboard), models changes, added in requirements.txt pacakge

--- backend/root/admin.py
+++ backend/root/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import BookInterview
+from .models import BookInterview, PaymentDetails
 
 admin.site.register(BookInterview)
+admin.site.register(PaymentDetails)

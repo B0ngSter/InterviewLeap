420391bbea5829c3e4c4afd1dfff3ed186d822ed
G0dlikecoder <55620900+G0dlikecoder@users.noreply.github.com>
2020-08-17 21:59:55 +0530

    dashboard and feedback changes

--- leap/pages/Dashboard.vue
+++ leap/pages/Dashboard.vue
@@ -1,6 +1,121 @@
 <template>
   <b-container>
-    <mockInterviewListing />
+    <b-row class="mt-5">
+      <b-col cols="12" md="3" class="pb-5 mb-5">
+        <h3 class="font-weight-bold">
+          Hello, {{ this.$store.state.auth.user.first_name }}
+        </h3>
+        <p v-if="$store.getters.is_interviewer" class="text-secondary">
+          Below are your insights
+        </p>
+      </b-col>
+      <b-col cols="12" md="9">
+        <b-button @click="router.push('/create-interview')">
+          Create an Interview
+        </b-button>
+      </b-col>
+      <b-col v-if="$store.getters.is_interviewer" cols="12" md="6" class="pa-5">
+        <b-card no-body class="text-center border-0">
+          <b-container class="bg-white">
+            <b-row>
+              <b-col :cols="this.$store.state.is_mock ? 3 : 4" class="pt-5 pb-5 pl-4">
+                <b-img
+                  src="@/static/interview_requests.svg"
+                  alt="InterviewLeap logo"
+                />
+              </b-col>
+              <b-col v-if="this.$store.state.is_mock" cols="3" class="pt-5 pb-5 pl-4">
+                <h4 class="text-left font-weight-bold">
+                  {{ interviewer_insights.new_interview_requests }}
+                </h4>
+                <p class="text-left text-secondary">
+                  New Interview Requests
+                </p>
+              </b-col>
+            </b-row>
+          </b-container>
+        </b-card>
+      </b-col>
+      <b-col v-if="$store.getters.is_interviewer" cols="12" md="6" class="pa-5">
+        <b-card no-body class="text-center border-0">
+          <b-container class="bg-white">
+            <b-row>
+              <b-col :cols="this.$store.state.is_mock ? 3 : 4" class="pt-5 pb-5 pl-4">
+                <b-img
+                  src="@/static/interview_taken.svg"
+                  alt="InterviewLeap logo"
+                />
+              </b-col>
+              <b-col v-if="this.$store.state.is_mock" cols="3" class="pt-5 pb-5 pl-4">
+                <h4 class="text-left font-weight-bold">
+                  {{ interviewer_insights.interview_taken }}
+                </h4>
+                <p class="text-left text-secondary">
+                  Total Interviews Taken
+                </p>
+              </b-col>
+            </b-row>
+          </b-container>
+        </b-card>
+      </b-col>
+      <b-col v-if="$store.getters.is_interviewer" cols="12" md="6" class="pa-5">
+        <b-card no-body class="text-center border-0">
+          <b-container class="bg-white">
+            <b-row>
+              <b-col :cols="this.$store.state.is_mock ? 3 : 4" class="pt-5 pb-5 pl-4">
+                <b-img
+                  src="@/static/interview_created.svg"
+                  alt="InterviewLeap logo"
+                />
+              </b-col>
+              <b-col v-if="this.$store.state.is_mock" cols="3" class="pt-5 pb-5 pl-4">
+                <h4 class="text-left font-weight-bold">
+                  {{ interviewer_insights.interview_created }}
+                </h4>
+                <p class="text-left text-secondary">
+                  Interviews Created by You
+                </p>
+              </b-col>
+            </b-row>
+          </b-container>
+        </b-card>
+      </b-col>
+      <b-col v-if="$store.getters.is_interviewer" cols="12" md="6" class="pa-5">
+        <b-card no-body class="text-center border-0">
+          <b-container class="bg-white">
+            <b-row>
+              <b-col :cols="this.$store.state.is_mock ? 3 : 4" class="pt-5 pb-5 pl-4">
+                <b-img
+                  src="@/static/earning.svg"
+                  alt="InterviewLeap logo"
+                />
+              </b-col>
+              <b-col v-if="this.$store.state.is_mock" cols="3" class="pt-5 pb-5 pl-4">
+                <h4 class="text-left font-weight-bold">
+                  {{ interviewer_insights.total_earnings }}
+                </h4>
+                <p class="text-left text-secondary">
+                  Total Earnings
+                </p>
+              </b-col>
+            </b-row>
+          </b-container>
+        </b-card>
+      </b-col>
+      <b-col v-if="$store.getters.is_interviewer" cols="12">
+        <h4 class="text-left font-weight-bold">
+          New Interview Requests
+        </h4>
+        <b-img
+          v-if="interviewer_insights.interview_requests == null"
+          src="@/static/blank_state_interviewer.svg"
+          alt="InterviewLeap logo"
+        />
+      </b-col>
+      <b-col v-if="$store.getters.is_candidate" cols="12">
+        <mockInterviewListing />
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -12,6 +127,18 @@ export default {
   },
   data () {
     return {
+      interviewer_insights: {
+        new_interview_requests: 1,
+        interview_created: 1,
+        interview_taken: 1,
+        total_earnings: 1,
+        interview_requests: {
+          applied_designation: null,
+          time_slots: [],
+          date: 'datetime',
+          candidate: 'FK'
+        }
+      }
     }
   }
 }
---
+++ leap/pages/feedback.vue
@@ -0,0 +1,130 @@
+<template>
+  <b-container>
+    <b-row align-v="start" align-content="start" class="flex-grow-1">
+      <b-col cols="12">
+        <b-breadcrumb class="bg-light pl-0">
+          <b-breadcrumb-item>
+            Dashboard
+          </b-breadcrumb-item>
+          <b-breadcrumb-item>
+            Interview Request
+          </b-breadcrumb-item>
+          <b-breadcrumb-item active>
+            Feedback
+          </b-breadcrumb-item>
+        </b-breadcrumb>
+      </b-col>
+      <b-col cols="12">
+        <h4 class="font-weight-bold">
+          Feedback
+        </h4>
+      </b-col>
+      <b-col cols="12" class="mt-5">
+        <b-card no-body class="text-center border-0">
+          <b-container class="bg-white">
+            <b-row>
+              <b-col cols="12" class="pt-5 pb-5 pl-4 border-bottom border-light">
+                <p class="text-left text-secondary">
+                  Date &amp; time
+                </p>
+                <h4 class="text-left text-dark font-weight-bold">
+                  <!-- {{ date() }} -->
+                </h4>
+              </b-col>
+              <b-col cols="5" class="pt-3 pb-5 pl-4">
+                <p class="text-left text-danger-dark  font-weight-bold">
+                  Role - Front-end developer
+                </p>
+              </b-col>
+              <b-col cols="4">
+                <div class="pt-3 mb-5">
+                  <p class="text-right font-weight-bold">
+                    View Candidate Profile >
+                  </p>
+                </div>
+              </b-col>
+              <b-col cols="3">
+                <div class="pt-3 mb-5">
+                  <p class="text-right font-weight-bold">
+                    Downloud Resume
+                  </p>
+                </div>
+              </b-col>
+            </b-row>
+          </b-container>
+        </b-card>
+      </b-col>
+      <b-col cols="12">
+        blah blah blah
+      </b-col>
+      <b-col cols="12" class="mt-5">
+        <b-card no-body class="text-center border-0">
+          <b-container class="bg-white">
+            <b-row>
+              <b-col cols="12" class="pt-5 pb-2 pl-4 border-bottom border-light">
+                <p class="text-left text-secondary">
+                  Technical Skill
+                </p>
+              </b-col>
+              <b-col cols="3" class="pt-3 pb-5">
+                <b-form-radio v-model="feedback.technical_skill" class="font-weight-bold" value="Exceptional" size="md">
+                  Exceptional
+                </b-form-radio>
+              </b-col>
+              <b-col cols="3 pt-3 pb-5">
+                <b-form-radio v-model="feedback.technical_skill" class="font-weight-bold" value="Meets Requirenment" size="md">
+                  Meets Requirenment
+                </b-form-radio>
+              </b-col>
+              <b-col cols="3 pt-3 pb-5">
+                <b-form-radio v-model="feedback.technical_skill" class="font-weight-bold" value="Need Training" size="md">
+                  Need Training
+                </b-form-radio>
+              </b-col>
+              <b-col cols="3 pt-3 pb-5">
+                <b-form-radio v-model="feedback.technical_skill" class="font-weight-bold" value="Doesn't meet requirenment" size="md">
+                  Doesn't meet requirenment
+                </b-form-radio>
+              </b-col>
+              <b-col v-if="feedback.technical_skill !== ''" cols="12">
+                <p class="text-left text-secondary">
+                  Detail Feedback
+                </p>
+                <b-form-textarea
+                  id="textarea"
+                  v-model="feedback.text"
+                />
+                <b-button variant="primary" class="mt-5">
+                  Save
+                </b-button>
+              </b-col>
+            </b-row>
+          </b-container>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+
+export default {
+  auth: 'guest',
+  data () {
+    return {
+      feedback: {
+        technical_skill: '',
+        communicational_skill: '',
+        presentation_skill: '',
+        understanding_of_role: '',
+        text: '',
+        strength: '',
+        limitations: '',
+        consider_for_job: ''
+      }
+    }
+  }
+}
+</script>
+<style>
+</style>
